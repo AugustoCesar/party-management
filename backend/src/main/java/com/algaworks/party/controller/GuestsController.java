101b0a365a8c922925422114dcd0f3f6a4ff9ad1
Augusto Cesar <augustocesar.udi@gmail.com>
2021-06-10 14:17:49 -0300

    GuestsController, html and dependencies

---
+++ backend/src/main/java/com/algaworks/party/controller/GuestsController.java
@@ -0,0 +1,13 @@
+package com.algaworks.party.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GuestsController {
+	
+	@GetMapping("/guests")
+	public String toList() {
+		return "GuestsList";
+	}
+}
