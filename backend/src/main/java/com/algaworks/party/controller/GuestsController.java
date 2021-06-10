23e0ad6b5b184013c1a481365710a2d89fc8cc76
Augusto Cesar <augustocesar.udi@gmail.com>
2021-06-10 16:36:53 -0300

    Add new guest

--- backend/src/main/java/com/algaworks/party/controller/GuestsController.java
+++ backend/src/main/java/com/algaworks/party/controller/GuestsController.java
@@ -3,23 +3,37 @@ package com.algaworks.party.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.party.model.Guest;
 import com.algaworks.party.model.GuestRepository;
 
 @Controller
+@RequestMapping("/guests")
 public class GuestsController {
 	
 	@Autowired
 	private GuestRepository guestsRepository;
 	
-	@GetMapping("/guests")
+	@GetMapping
 	public ModelAndView toList() {
 		
 		ModelAndView modelAndView = new ModelAndView("GuestsList");
-		
+				
 		modelAndView.addObject("guests", guestsRepository.findAll());
 		
+		modelAndView.addObject(new Guest());
+		
 		return modelAndView;
+	}
+	
+	@PostMapping
+	public String save(Guest guest) {
+		
+		this.guestsRepository.save(guest);
+		
+		return "redirect:/guests";
 	}
 }
